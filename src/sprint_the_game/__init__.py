26d09b25970ffd950ae46eebd8ce33c5136744e4
Hennzau <dev@enzo-le-van.fr>
2024-12-21 12:22:45 +0100

    rework

--- src/sprint_the_game/__init__.py
+++ src/sprint_the_game/__init__.py
@@ -1,43 +1,20 @@
 import pyxel
 
-from sprint_the_game.level_editor import LeveLEditor
-from sprint_the_game.main_menu import MainMenu
-from sprint_the_game.playing import Playing
-from sprint_the_game.state import GameState
-from sprint_the_game.victory import Victory
-
 
 class App:
     def __init__(self):
         pyxel.init(256, 144, title="Sprint The Game", display_scale=5)
 
-        self.state = GameState.MAIN_MENU
-
         pyxel.load("../../my_resource.pyxres")
-
-        self.game = {
-            GameState.MAIN_MENU: MainMenu(),
-            GameState.LEVEL_EDITOR: LeveLEditor(),
-            GameState.PLAYING: Playing(),
-            GameState.VICTORY: Victory(),
-        }
 
     def run(self):
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        next_state = self.game[self.state].update()
-
-        if next_state == GameState.LEAVING:
-            pyxel.quit()
-            return
-
-        self.state = next_state
+        pass
 
     def draw(self):
         pyxel.cls(0)
-
-        self.game[self.state].draw()
 
 
 def main() -> None:
--- src/sprint_the_game/event.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class GameEvent(Enum):
-    MAIN_MENU_TO_PLAYING = (1,)
-    MAIN_MENU_TO_LEAVE = (2,)
-    MAIN_MENU_TO_OPTIONS = (3,)
